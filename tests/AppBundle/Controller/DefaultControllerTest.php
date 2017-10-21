c3f68645394d20c4e2294788f01981c57629a9e3
LoickVirot <virot.loick@gmail.com>
2017-10-21 10:58:32 +0200

    add static url generation test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,16 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private $client;
+    private $em;
+    private $container;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->client = static::createClient(array('environment' => 'test'));
+        $this->container = $this->client->getContainer();
+        $this->em = $this->container->get('doctrine')->getManager();
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
+        $crawler = $this->client->request('GET', '/unittest');
 
-        $crawler = $client->request('GET', '/unittest');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertContains(
             'this is the unit test page',
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function testStaticPageUrlGeneration()
+    {
+        $twig = $this->container->get('twig');
+        $globals = $twig->getGlobals();
+        $url = $globals['prefix_static_link'] . "unittest";
+
+        $this->assertEquals("/unittest", $url);
+        $crawler = $this->client->request('GET', $url);
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertContains(
+            'this is the unit test page',
+            $this->client->getResponse()->getContent()
         );
     }
 }
